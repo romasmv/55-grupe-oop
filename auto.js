acbb344744f03224ae8ea27fd426eca7cad9b7ad
Roman <rmorozovas@gmail.com>
2025-04-14 11:19:19 +0300

    update Car && auto

--- auto.js
+++ auto.js
@@ -1,7 +1,14 @@
+
 import{ Car } from './js/Car.js';
 
 
-const volvo =  new Car();
+const volvo = new Car('Volvo S40', 'red', 55);
+const zapas = new Car('Zapas', 'white', 35);
+console.log(volvo.intro());
+console.log(zapas.intro());
+zapas.refill(20);
+console.log(zapas);
+
 
 
 
--- js/Car.js
+++ js/Car.js
@@ -1,18 +1,42 @@
+/*
+intro - apibudina masinos modeli ir spalva
+drive() - nurodzius norima nuvaziuoti atstuma, yra paskaiciuojamos degalu sanaudos, jei nepakanka degalu,  tai ribiniu atveju teisiog sustoja ir masina issijungia
+engineOn() - ijungia varikli, jei yra kuro ir jei yra ijungtas variklis
+engineOff() - isjungia varikli, jei variklis yra ijungtas
+refill() - uzpilti degalu, bet ne daugiau, nei telpa ir jei variklis yra isjungtas
+
+*/
+
+
 export class Car {
-    constructor(make, model, year) {
-        this.make = make;
+    constructor(model, color, tankMax) {
         this.model = model;
-        this.year = year;
-    }
+        this.color = color;
+        this.tankMax = tankMax;
+        this.tank = 0;
+        this.engineIsOn = false;
     
-    getCarInfo() {
-        return `${this.year} ${this.make} ${this.model}`;
     }
+    intro() {
+        return `This is a ${this.color} ${this.model}`;   
+    }
+    refill(volue) {
+        if (this.engineIsOn) {
+            return `Error: to refill the tank, you must turn off the engine first`;
+        }
+        if(typeof volue !== 'number' || !isFinite(volue)) {
+            return `Error: Only real numbers are allowed`;
+        }
+        if (volue < 0) {
+            return `Refill with ${volue} litres is not allowed`;
+        }
+        if (this.tankMax - this.tank >= volue) {
+            this.tank += volue;
+        } else {
+            this.tank = this.tankMax;
+        }
+      }
 }
-
-const car1 = new Car('Toyota', 'Corolla', 2020);
-
-
 
 
 
